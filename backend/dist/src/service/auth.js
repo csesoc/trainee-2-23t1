1c66b4631c971bf79abf62acaa83a0efe286b027
m4ch374 <spiner47@gmail.com>
2023-04-01 22:25:34 +1100

    resolved conflict

--- backend/dist/src/service/auth.js
+++ backend/dist/src/service/auth.js
@@ -8,64 +8,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const provider_1 = require("../utils/provider");
+const trpc_provider_1 = require("../trpc_provider");
 const zod_1 = require("zod");
-const provider_2 = require("../utils/provider");
-const server_1 = require("@trpc/server");
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const config_1 = require("../../config");
-const signToken = (usrId) => jsonwebtoken_1.default.sign({ userId: usrId }, config_1.JwtSecret, { algorithm: "HS256" });
-const loginEndpoint = provider_1.trpc.procedure.input(zod_1.z.object({
-    email: zod_1.z.string(),
-    password: zod_1.z.string(),
-})).output(zod_1.z.object({
-    token: zod_1.z.string(),
-})).mutation(({ input }) => __awaiter(void 0, void 0, void 0, function* () {
-    const usr = yield provider_2.prisma.user.findFirst({
-        where: {
-            email: input.email,
-            password: input.password
-        }
-    });
-    if (typeof usr === 'undefined' || usr == null) {
-        throw new server_1.TRPCError({
-            code: 'NOT_FOUND',
-            message: "Email or password not found",
-        });
-    }
-    else {
-        return { token: signToken(usr.id) };
-    }
-}));
-const registerEndpoint = provider_1.trpc.procedure.input(zod_1.z.object({
-    name: zod_1.z.string(),
-    email: zod_1.z.string(),
-    password: zod_1.z.string(),
-    handle: zod_1.z.string()
-})).output(zod_1.z.object({
-    token: zod_1.z.string(),
-})).mutation(({ input }) => __awaiter(void 0, void 0, void 0, function* () {
-    const usr = yield provider_2.prisma.user.create({
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+const loginEndpoint = trpc_provider_1.trpc.procedure.input(zod_1.z.object({
+    username: zod_1.z.string(),
+}))
+    .mutation(({ input }) => __awaiter(void 0, void 0, void 0, function* () {
+    const usr = yield prisma.user.create({
         data: {
-            name: input.name,
-            email: input.email,
-            password: input.password,
-            handle: input.handle,
+            name: input.username,
+            email: "email@email.com",
+            password: "password"
         }
-    }).catch(() => {
-        throw new server_1.TRPCError({
-            code: 'INTERNAL_SERVER_ERROR',
-            message: "Something went wrong in the server",
-        });
     });
-    return { token: signToken(usr.id) };
+    return `Hello ${usr.name} with id ${usr.id}`;
 }));
-const authRouter = provider_1.trpc.router({
+const authRouter = trpc_provider_1.trpc.router({
     login: loginEndpoint,
-    register: registerEndpoint,
 });
 exports.default = authRouter;
