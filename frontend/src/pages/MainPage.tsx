b916da8f29d44ac2427cc960f43212fbcaa52f27
Dylan Huynh <54874111+TAS-scorchedshadow@users.noreply.github.com>
2023-06-03 23:02:19 +1000

    Added TimeslotAdder (#27)

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import CalendarTest from "./CalendarTest";
+import TimeslotAdder from "../components/Calendar/TimeslotAdder";
 
 
 const MainPage: React.FC = () => {
+  const [trigger, setTrigger] = useState(true)
   return (
-    <div className="flex justify-around items-center w-screen">
+    <div className="flex justify-around items-center">
       <div className="bg-gray-500 w-56 h-56"></div>
+      <div>
+        <TimeslotAdder calendarId="646cb1da47ac2c8b59c89bf1" trigger={trigger} setTrigger={setTrigger}/>
+      </div>
       <div className="w-[45%] mt-5">
-        <CalendarTest/>
+        <CalendarTest calendarId="646cb1da47ac2c8b59c89bf1" trigger={trigger} setTrigger={setTrigger}/>
       </div>
     </div>
   )
