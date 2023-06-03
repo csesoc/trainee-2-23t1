e112f2db8453c5f63122f34d52519602bb5b6c6a
Dylan <redman29h@gmail.com>
2023-06-04 00:30:10 +1000

    Now draws from context

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,19 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import CalendarTest from "./CalendarTest";
 import TimeslotAdder from "../components/Calendar/TimeslotAdder";
+import { UserIdProvider } from "../components/ProtectedRoutes";
+import { trpc } from "../utils/trpc";
 
 
 const MainPage: React.FC = () => {
   const [trigger, setTrigger] = useState(true)
+  const selfId = useContext(UserIdProvider)
+  
+  const r = trpc.user.getUserProfile.useQuery({token: selfId});
+  const userInfo = r.data
+  if (r.isLoading) {
+    return(<div></div>)
+  }
   return (
     <div className="flex justify-around items-center">
       <div className="bg-gray-500 w-56 h-56"></div>
       <div>
-        <TimeslotAdder calendarId="646cb1da47ac2c8b59c89bf1" trigger={trigger} setTrigger={setTrigger}/>
+        <TimeslotAdder calendarId={userInfo.calendarId} trigger={trigger} setTrigger={setTrigger}/>
       </div>
       <div className="w-[45%] mt-5">
-        <CalendarTest calendarId="646cb1da47ac2c8b59c89bf1" trigger={trigger} setTrigger={setTrigger}/>
+        <CalendarTest calendarId={userInfo.calendarId} trigger={trigger} setTrigger={setTrigger}/>
       </div>
     </div>
   )
